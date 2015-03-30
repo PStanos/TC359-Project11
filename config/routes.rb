f9c072d5da371fdefecadd68bad765ab2a2759b5
PStanos <paulstanos@gmail.com>
2015-03-30 15:47:44 -0400

    +Routes; +Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/sign-in' => 'sessions#new'
+  delete '/sign-out' => 'sessions#destroy'
   get 'static/home'
 
   root 'static#home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323184832) do
+ActiveRecord::Schema.define(version: 20150330194353) do
 
   create_table "cats", force: :cascade do |t|
     t.text     "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150323184832) do
     t.boolean  "declawed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
